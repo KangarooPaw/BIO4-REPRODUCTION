a0361b5056792f5b84e0c636437e2bc3892b5745
KangarooPaw <eva.16shito.kanata@gmail.com>
2020-10-22 14:27:08 +0900

    a

---
+++ 01_project/02_BIOHAZRD4/BIOHAZRD4/light.cpp
@@ -0,0 +1,41 @@
+#include "main.h"
+#include "manager.h"
+#include "renderer.h"
+#include "light.h"
+
+CLight::CLight()
+{
+
+}
+
+CLight::~CLight()
+{
+
+}
+
+CLight * CLight::Create(void)
+{
+	CLight*pLight;
+	pLight = new CLight;
+	pLight->Init();
+	return pLight;
+}
+
+void CLight::Init(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = CManager::GetRenderer()->GetDevice();
+	D3DXVECTOR3 vecDir;
+	ZeroMemory(&m_light, sizeof(D3DLIGHT9));
+	m_light.Type = D3DLIGHT_DIRECTIONAL;
+	m_light.Diffuse = D3DXCOLOR(1, 1, 1, 1);
+	vecDir = D3DXVECTOR3(0.2f, -0.8f, -0.4f);
+	D3DXVec3Normalize(&vecDir, &vecDir);
+	m_light.Direction = vecDir;
+	pDevice->SetLight(0, &m_light);
+	pDevice->LightEnable(0, TRUE);
+}
+
+void CLight::Uninit(void)
+{
+
+}
