d916a8681cf5dc247c8d0059eabdcecba315f72c
KangarooPaw <eva.16shito.kanata@gmail.com>
2020-11-03 22:53:00 +0900

    紛失したエネミーの復活

---
+++ 01_project/02_BIOHAZARD4/02_BIOHAZARD4/enemy.cpp
@@ -0,0 +1,274 @@
+//****************************************
+//モデル処理
+//****************************************
+
+//----------------------------------------
+//インクルードファイル
+//----------------------------------------
+#include "main.h"
+#include "manager.h"
+#include "renderer.h"
+#include "joystick.h"
+#include "enemy.h"
+#include "motion.h"
+#include "model.h"
+#include "player.h"
+#include "game.h"
+
+//----------------------------------------
+//静的メンバ変数
+//----------------------------------------
+LPD3DXMESH CEnemy::m_pMesh[MAX_ENEMY_PARTS] = {};
+LPD3DXBUFFER CEnemy::m_pBuffMat[MAX_ENEMY_PARTS] = {};
+DWORD CEnemy::m_nNumMat[MAX_ENEMY_PARTS] = {};
+D3DXMATRIX CEnemy::m_mtxWorld[MAX_ENEMY_PARTS] = {};	 // 行列計算用
+int CEnemy::m_nldxModelParent[MAX_ENEMY_PARTS] = {};	 // 親モデルのインデックス
+char* CEnemy::m_apFileName[MAX_ENEMY_PARTS] = {
+    { "data/MODEL/ENEMY/body.x" },			// 上半身
+    { "data/MODEL/ENEMY/bodyUnder.x" },	// 下半身
+    { "data/MODEL/ENEMY/head.x" },			// 頭
+    { "data/MODEL/ENEMY/legMomoLeft.x" },	// 左もも
+    { "data/MODEL/ENEMY/legLeft.x" },		// 左足
+    { "data/MODEL/ENEMY/legMomoRight.x" }, // 右もも
+    { "data/MODEL/ENEMY/legRight.x" },		// 右足
+    { "data/MODEL/ENEMY/upArmLeft.x" },    // 左上腕
+    { "data/MODEL/ENEMY/downArmLeft.x" },  // 左前腕
+    { "data/MODEL/ENEMY/handLeft.x" },	    // 左手
+    { "data/MODEL/ENEMY/upArmRight.x" },   // 右上腕
+    { "data/MODEL/ENEMY/downArmRight.x" }, // 右前腕
+    { "data/MODEL/ENEMY/handRight.x" },    // 右手
+};
+bool CEnemy::m_bChase = false;
+LPDIRECT3DTEXTURE9 CEnemy::m_pTexture[MAX_ENEMY_PARTS] = {};
+
+//----------------------------------------
+//インクリメント
+//----------------------------------------
+CEnemy::CEnemy(int nPriority) :CScene(nPriority)
+{
+    m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+    m_rot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+    m_size = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_nCntFrame = 0;
+}
+
+//----------------------------------------
+//デクリメント
+//----------------------------------------
+CEnemy::~CEnemy()
+{
+
+}
+
+//----------------------------------------
+//生成処理
+//----------------------------------------
+CEnemy * CEnemy::Create(D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR3 size)
+{
+    CEnemy *pEnemy;
+    pEnemy = new CEnemy;
+    pEnemy->SetEnemy(pos, rot, size);
+    pEnemy->Init();
+    return pEnemy;
+}
+
+//----------------------------------------
+//モデルの読み込み
+//----------------------------------------
+HRESULT CEnemy::Load(void)
+{
+    LPDIRECT3DDEVICE9 pDevice = CManager::GetRenderer()->GetDevice();
+
+    for (int nCount = 0; nCount < MAX_ENEMY_PARTS; nCount++)
+    {
+        // Xファイルの読み込み
+        D3DXLoadMeshFromX(m_apFileName[nCount],
+            D3DXMESH_SYSTEMMEM,
+            pDevice,
+            NULL,
+            &m_pBuffMat[nCount],
+            NULL,
+            &m_nNumMat[nCount],
+            &m_pMesh[nCount]
+        );
+
+		D3DXCreateTextureFromFile(pDevice,
+			m_apFileName[nCount],
+			&m_pTexture[nCount]);
+    }
+
+    return E_NOTIMPL;
+}
+
+//----------------------------------------
+//モデルの破棄
+//----------------------------------------
+void CEnemy::Unload(void)
+{
+    for (int nCount = 0; nCount < MAX_ENEMY_PARTS; nCount++)
+    {
+        // メッシュの破棄
+        if (m_pMesh[nCount] != NULL)
+        {
+            m_pMesh[nCount]->Release();
+            m_pMesh[nCount] = NULL;
+        }
+
+        // マテリアルの破棄
+        if (m_pBuffMat[nCount] != NULL)
+        {
+            m_pBuffMat[nCount]->Release();
+            m_pBuffMat[nCount] = NULL;
+        }
+
+        if (m_nNumMat[nCount] != NULL)
+        {
+            m_nNumMat[nCount] = NULL;
+        }
+    }
+
+}
+
+//----------------------------------------
+//初期化処理
+//----------------------------------------
+HRESULT CEnemy::Init(void)
+{
+	D3DXMATRIX mtxRot, mtxTrans;
+
+	// モーションの生成
+	m_pMotion = CMotion::Create();
+
+	// モーションの読み込み
+    m_pMotion->Load(LOAD_ENEMY_TEXT);
+    m_pMotion->LoadMotion(MOTION_ENEMY_TEXT);
+
+	// モーションの初期設定
+	m_pMotion->SetMotion(CMotion::MOTION_IDLE);
+
+	for (int nCount = 0; nCount < MAX_ENEMY_PARTS; nCount++)
+	{
+		// モデルの生成
+		m_pModel[nCount] = CModel::Create();
+
+		// ペアレントの受け取り
+		m_nldxModelParent[nCount] = m_pMotion->GetParents(nCount);
+
+		// モデルのパーツごとの座標と回転を受け取る
+		m_pModel[nCount]->SetModel(m_pMotion->GetPos(nCount), m_pMotion->GetRot(nCount), m_size);
+
+		// モデルのバインド
+		m_pModel[nCount]->BindModel(m_pMesh[nCount], m_pBuffMat[nCount], m_nNumMat[nCount], m_nldxModelParent[nCount], m_pTexture[nCount]);
+	}
+
+	// 座標、回転、サイズのセット
+	m_pModel[0]->SetModel(m_pMotion->GetPos(0) + m_pos, m_pMotion->GetRot(0) + m_rot, m_size);
+
+    m_bChase = false;
+    return S_OK;
+}
+
+//----------------------------------------
+//終了処理
+//----------------------------------------
+void CEnemy::Uninit(void)
+{
+	for (int nCount = 0; nCount < MAX_ENEMY_PARTS; nCount++)
+	{
+		if (m_pModel[nCount] != NULL)
+		{
+			// モデルクラスの終了処理
+			m_pModel[nCount]->Uninit();
+			m_pModel[nCount] = NULL;
+		}
+	}
+	if (m_pMotion != NULL)
+	{
+		// モーションクラスの終了処理
+		m_pMotion->Uninit();
+		m_pMotion = NULL;
+	}
+	Release();
+}
+
+//----------------------------------------
+//更新処理
+//----------------------------------------
+void CEnemy::Update(void)
+{
+	// モーションの更新処理
+	m_pMotion->UpdateMotion();
+	//プレイヤーの場所の取得
+	D3DXVECTOR3 pPlayerPos = CGame::GetPlayer()->GetPos();
+	if (m_bChase == false)
+	{
+		//モーションセット(走る)
+		m_pMotion->SetMotion(CMotion::MOTION_RUN);
+
+		//向いている方向に進む
+		m_pos.x += -sinf(m_rot.y)*0.5f;
+		m_pos.z += -cosf(m_rot.y)*0.5f;
+
+		//2秒に1回90度回転
+		m_nCntFrame++;
+		if (m_nCntFrame % 120 == 0)
+		{
+			m_rot.y += D3DXToRadian(90);
+		}
+		if (m_pos.x - pPlayerPos.x >= -50 && m_pos.x - pPlayerPos.x <= 50 &&
+			m_pos.z - pPlayerPos.z >= -50 && m_pos.z - pPlayerPos.z <= 50)
+		{
+			m_bChase = true;
+		}
+	}
+	else
+	{	
+		float angle = (float)atan2( pPlayerPos.x - m_pos.x,pPlayerPos.z - m_pos.z);
+		m_rot.y =angle-D3DXToRadian(180);
+		//向いている方向に進む
+		m_pos.x += -sinf(m_rot.y)*0.5f;
+		m_pos.z += -cosf(m_rot.y)*0.5f;
+	}
+	for (int nCount = 0; nCount < MAX_ENEMY_PARTS; nCount++)
+	{
+		// モデルのパーツごとのモーションの座標と回転を受け取る
+		m_pModel[nCount]->SetModel(m_pMotion->GetPos(nCount), m_pMotion->GetRot(nCount), m_size);
+	}
+
+	// 座標、回転、サイズのセット(親のモデルだけ動かすため)
+	m_pModel[0]->SetModel(m_pMotion->GetPos(0) + m_pos, m_pMotion->GetRot(0) + m_rot, m_size);
+}
+
+//----------------------------------------
+//描画処理
+//----------------------------------------
+void CEnemy::Draw(void)
+{
+	D3DXMATRIX mtxRot, mtxTrans;
+
+	for (int nCount = 0; nCount < MAX_ENEMY_PARTS; nCount++)
+	{
+		//ワールドマトリクスの初期化
+		D3DXMatrixIdentity(&m_mtxWorld[nCount]);
+
+		//向きを反映
+		D3DXMatrixRotationYawPitchRoll(&mtxRot, m_rot.y, m_rot.x, m_rot.z);
+		D3DXMatrixMultiply(&m_mtxWorld[nCount], &m_mtxWorld[nCount], &mtxRot);
+
+		//位置を反映
+		D3DXMatrixTranslation(&mtxTrans, m_pos.x, m_pos.y, m_pos.z);
+		D3DXMatrixMultiply(&m_mtxWorld[nCount], &m_mtxWorld[nCount], &mtxTrans);
+
+		// ワールドマトリックスの設定
+		//m_pModel[nCount]->SetWorldMatrix(m_mtxWorld[nCount]);
+
+		// 親のモデルパーツ以外のペアレントをセット
+		if (m_nldxModelParent[nCount] != -1)
+		{
+			m_pModel[nCount]->SetParent(m_pModel[m_nldxModelParent[nCount]]);
+		}
+
+		// モデルクラスの描画処理
+		m_pModel[nCount]->Draw();
+	}
+}
