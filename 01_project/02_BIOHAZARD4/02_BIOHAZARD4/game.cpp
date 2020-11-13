ffe92519123d76b76931206167c1fb36663aa462
管原司 <stjbi99956@gmail.com>
2020-11-13 19:16:41 +0900

    a

--- 01_project/02_BIOHAZARD4/02_BIOHAZARD4/game.cpp
+++ 01_project/02_BIOHAZARD4/02_BIOHAZARD4/game.cpp
@@ -81,6 +81,7 @@ HRESULT CGame::Init(void)
 
 	// 門生成
 	m_pGate = m_pGate = CGate::Create(D3DXVECTOR3(-180.0f, 50.0f, 1060.0f), D3DXVECTOR3(0.0f, 0.0f, 0.0f), D3DXVECTOR3(100.0f, 100.0f, 20.0f));
+
 	//ボックス
 	for (int nCountX = 1; nCountX < 5; nCountX++)
 	{
